89f8bb0d37a81b2bd44451c669e4136e94cdda7f
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-09-17 18:11:28 +0900

    why not render ?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import { useContext, useEffect } from "react";
 import FixNavigation from "../components/FixNavigation";
 import HeadTag from "../components/HeadTag";
 import Loading from "../components/Loading";
 import Main from "../components/Main";
+import { NaviContext } from "../context/NaviContext";
 
 export default function Home() {
+  const [ref] = useContext(NaviContext);
+  // ページ内スクロール
+  useEffect(() => {
+    const newElm: HTMLElement = document.getElementById(ref)!;
+    if (!newElm) return;
+    const time: number = window.setTimeout(() => {
+      window.scrollTo({
+        top: newElm.offsetTop,
+        behavior: "smooth",
+        left: 0,
+      });
+    }, 1);
+    return () => window.clearTimeout(time);
+  }, [ref]);
   return (
     <>
       <HeadTag />
