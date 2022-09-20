1cccc0302f5b71962162876678875841dc0c4830
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-09-20 11:26:06 +0900

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import { useContext, useEffect } from "react";
 import FixNavigation from "../components/FixNavigation";
 import Loading from "../components/Loading";
 import Main from "../components/Main";
-import { NaviContext } from "../context/NaviContext";
 
 export default function Home() {
   return (
